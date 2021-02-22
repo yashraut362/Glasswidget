bfa6c084ae9a31a29d34316151a6c6c5e1080bcd
yashraut362 <yashraut362@gmail.com>
2021-02-22 15:52:14 +0530

    added simple example

---
+++ example/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:glasswidget/glasswidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: Text('GlassWidget'),
+          centerTitle: true,
+        ),
+        body: Center(
+
+
+          
+          child: GlassContainer(
+            glasscurve: 30, //giving border curve to glassmorphic container
+            glassheight: 200, //giving height to glassmorphic container
+            glasswidth: 300, //giving width to glassmorphic container
+            glasswidget: //Child widget u can use any flutter widget over here.
+                Center(
+              child: Text(
+                'Hello World',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+
+
+
+        ),
+      ),
+    );
+  }
+}
--- lib/src/glasscontainer.dart
+++ lib/src/glasscontainer.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class GlassContainer extends StatefulWidget {
   GlassContainer({
     Key key,
-    this.glassborder = 0.0,
+    this.glasscurve = 0.0,
     this.glassheight,
     this.glasswidth,
     this.glasswidget,
@@ -12,7 +12,7 @@ class GlassContainer extends StatefulWidget {
 
   final double glassheight;
   final double glasswidth;
-  final double glassborder;
+  final double glasscurve;
   final Widget glasswidget;
 
   @override
@@ -30,7 +30,7 @@ class _GlassContainerState extends State<GlassContainer> {
             color: Colors.black.withOpacity(0.2))
       ]),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(widget.glassborder),
+        borderRadius: BorderRadius.circular(widget.glasscurve),
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 26.0, sigmaY: 26.0),
           child: Container(
@@ -38,7 +38,7 @@ class _GlassContainerState extends State<GlassContainer> {
               width: widget.glasswidth,
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(0.15),
-                borderRadius: BorderRadius.circular(widget.glassborder),
+                borderRadius: BorderRadius.circular(widget.glasscurve),
                 border: Border.all(
                   width: 1.5,
                   color: Colors.white.withOpacity(0.2),
